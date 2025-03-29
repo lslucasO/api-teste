94d09fb91ef90c0bba590b41fe867bbba3f9470c
lslucaso <lslemos59@gmail.com>
2025-03-29 12:40:00 -0300

    #

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import views
-from backend import settings
-
 
 urlpatterns = [
-    path('api/v1/', views.ProductListView.as_view(), name='products_api_v1')
+    path('api/v1/', views.ProductListAPIView.as_view(), name='products_api_v1'),
+    path('api/v1/<int:pk>/', views.ProductRetriveAPIView.as_view(), name='products_api_v1'),
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, media_root=settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from products.models import Product
 from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 
 
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-class ProductListView(ListAPIView):
+
+class ProductRetriveAPIView(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
